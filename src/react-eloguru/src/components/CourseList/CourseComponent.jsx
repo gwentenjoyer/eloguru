71a70fcc1e2f8481fb4d95df2d6af8cdacd60315
Mykhailo Kapusniak <mykhailo.k@saasjet.com>
2024-06-05 07:03:02 +0300

    courseList fix current page, add bootstrap scripts,

--- src/react-eloguru/src/components/CourseList/CourseComponent.jsx
+++ src/react-eloguru/src/components/CourseList/CourseComponent.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import '../../css/Comment.css'
 function CourseComponent({ item }) {
+    const printRate = (value) => {
+        // const num = value.
+        return parseFloat(value).toFixed(1);
+    }
     return (
         <div className="card m-1 d-flex flex-column align-items-center h-100 productInstance">
             <div className="d-flex justify-content-center photo-cont">
@@ -8,8 +12,9 @@ function CourseComponent({ item }) {
 
             </div>
             <div className="card-body d-flex flex-column align-items-center">
-                <h5 className="card-title card-model-title">${item?.header + " " + item?.rating}</h5>
-                <p className="card-text card-model-version">${item?.duration} days</p>
+                <h5 className="card-title card-model-title">{item?.header}</h5>
+                <h6 className="card-title card-model-title">Rating: {printRate(item?.rating)} / 5</h6>
+                <p className="card-text card-model-version">{item?.durationDays} days</p>
             </div>
         </div>
     );
--- src/react-eloguru/src/components/CourseList/CourseList.jsx
+++ src/react-eloguru/src/components/CourseList/CourseList.jsx
@@ -1,53 +1,77 @@
-import { Pagination } from "flowbite-react";
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../css/CourseList.css';
-import Comment from "../CoursePage/Comment";
 import CourseComponent from "./CourseComponent";
+import { Pagination } from "flowbite-react";
 
 const CourseList = () => {
-    // const [activeTab, setActiveTab] = useState('info');
-    // const [activeTopic, setActiveTopic] = useState('info');
     const [firstPage, setFirstPage] = useState();
     const [isLoading, setIsLoading] = useState(true);
     const [currentPage, setCurrentPage] = useState(1);
+    const [pageSize, setPageSize] = useState(2);
 
-    // setCourse(test)
+    const fetchUserInfo = async (page, size) => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/courses?page=${page}&size=${size}`, { credentials: 'include' });
+            const data = await response.json();
+            console.log(data)
+            setFirstPage(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     useEffect(() => {
-            const fetchUserInfo = async () => {
-                const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/courses`, {credentials: 'include'});
-                const data = await response.json();
-                console.log(data);
-                setFirstPage(data)
-                setIsLoading(false);
-            };
-            fetchUserInfo();
-        },
-        []);
+        fetchUserInfo(currentPage - 1, pageSize);
+    }, [currentPage, pageSize]);
 
-    const onPageChange = (page) => setCurrentPage(page);
-
-
-
+    const onPageChange = (page) => {
+        setCurrentPage(page);
+    };
 
     return (
         <div>
+            <div className="mainwrapper d-flex flex-row pt-4 mb-3">
+                <aside className="catalog-sidebar me-4 ms-3">
+                    <div className="sidebar-title">
+                        <b>Критерії вибору товару</b>
+                        <hr/>
 
-            { isLoading ? <div>Loading...</div> :
-                // <div></div>
-            <div className="cards-container d-flex flex-wrap mt-1">
-
-                {firstPage?.content.map((item, index) => (
-                    <CourseComponent item={item}></CourseComponent>
-                ))}
+                        <hr/>
+                        <b className="sidebar-parag-title">Сортувати за:</b>
+                        <select className="form-select" id="sortby" aria-label="Default select example"
+                            // onChange="sortPhones()"
+                        >
+                            <option value="0" key={0}>Старішим</option>
+                            <option value="-1" key={-1}>Новішим</option>
+                            <option value="1" key={1}>Зростанням</option>
+                            <option value="2" key={2}>Спаданням</option>
+                        </select>
+                    </div>
+                </aside>
+                {isLoading ? <div>Loading...</div> :
+                    <div>
+                        <div className="cards-container d-flex flex-wrap mt-1">
+                            {firstPage?.content.map((item, index) => (
+                                <CourseComponent item={item} key={index}></CourseComponent>
+                            ))}
+                        </div>
+                        <div className="flex courses-list-nav justify-content-center">
+                            <Pagination
+                                className="flex flex-row"
+                                currentPage={currentPage}
+                                totalPages={firstPage?.totalPages || 1}
+                                onPageChange={onPageChange}
+                                showIcons
+                            />
+                        </div>
+                    </div>
+                }
             </div>
-                // <div className="flex courses-list-nav justify-content-center">
-                //     <Pagination className="flex flex-row" currentPage={currentPage} totalPages={firstPage.totalPages}
-                //                 onPageChange={onPageChange} showIcons/>
-                // </div>
-            }
         </div>
-
     );
-};
+}
+;
 
 export default CourseList;
