75fbf5c638b2ee7a69d647901e239bc8846c2e42
Ihor Yaremii <ihor.y@saasjet.com>
2024-06-19 21:03:31 +0300

    add @responseBody

--- eloguru/src/main/java/ua/thecompany/eloguru/controllers/SpringDefaultUrlController.java
+++ eloguru/src/main/java/ua/thecompany/eloguru/controllers/SpringDefaultUrlController.java
@@ -2,20 +2,25 @@ package ua.thecompany.eloguru.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SpringDefaultUrlController {
 	@GetMapping("/login")
+	@ResponseBody
 	public String showLoginPage() { return ""; }
 	@GetMapping("/sign-up")
+	@ResponseBody
 	public String showSignUpPage() { return ""; }
 	@GetMapping("/profile")
+	@ResponseBody
 	public String showProfilePage() { return ""; }
 	@GetMapping("/about")
 	public String showAboutPage() {
 		return "";
 	}
 	@GetMapping("/createCourse")
+	@ResponseBody
 	public String showTeachersPage() {
 		return "";
 	}
@@ -28,9 +33,11 @@ public class SpringDefaultUrlController {
 		return "";
 	}
 	@GetMapping("/activate")
+	@ResponseBody
 	public String showActivatePage() { return ""; }
 	@GetMapping("/course/:id")
 	public String showQuizPage() { return ""; }
 	@GetMapping("/admin")
+	@ResponseBody
 	public String showAdminPage() { return ""; }
 }
