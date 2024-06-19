fdb10ed69c756afb2f15911e21aea27206bc845a
Ihor Yaremii <ihor.y@saasjet.com>
2024-06-19 20:47:13 +0300

    fix requireAuth, add redirect for modal

--- react-eloguru/src/hooks/getAuth.js
+++ react-eloguru/src/hooks/getAuth.js
@@ -3,31 +3,28 @@ import {Outlet} from 'react-router-dom';
 // import getRefreshTokens.js from "./getRefreshTokens.js";
 
 export default function RequireAuth({url}) {
-    // const [isOk, setIsOk] = useState(null);
-    // const [isFinal, setIsFinal] = useState(null);
-    //
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         const response = await fetch(`${process.env.REACT_APP_BASE_URL}/${url}`,{credentials: 'include'});
-    //         if (response.status === 200 && !response.redirected) {
-    //             setIsOk(true);
-    //         // } else if (await getRefreshTokens.js() === true) {
-    //         //     await fetchData()
-    //         } else {
-    //             setIsFinal(true);
-    //         }
-    //     };
-    //     fetchData();
-    // }, [url]);
-    //
-    // if (isFinal) {
-    //     // window.location.href = "login";
-    //     console.log("that's bad auth")
-    // }
-    // if (isOk === null) {
-    //     return <div>Loading...</div>;
-    // }
-    //
-    // return isOk ? <Outlet/> : <div></div>;
-    return <Outlet/>;
+    const [isOk, setIsOk] = useState(null);
+    const [isFinal, setIsFinal] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`${process.env.REACT_APP_BASE_URL}/${url}`,{credentials: 'include'});
+            if (response.status === 200 && !response.redirected) {
+                setIsOk(true);
+            } else {
+                setIsFinal(true);
+            }
+        };
+        fetchData();
+    }, [url]);
+
+    if (isFinal) {
+        window.location.href = "/?login=true";
+        console.log("that's bad auth")
+    }
+    if (isOk === null) {
+        return <div>Loading...</div>;
+    }
+
+    return isOk ? <Outlet/> : <div></div>;
 }
